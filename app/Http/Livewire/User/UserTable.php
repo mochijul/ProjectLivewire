825607ea7a582164114ff85472c9dd81635064f7
Mochamad Hijul M <32985689+mochijul@users.noreply.github.com>
2023-03-27 22:50:08 +0700

    add view stucture

---
+++ app/Http/Livewire/User/UserTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class UserTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.user-table');
+    }
+}
