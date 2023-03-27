4706d6699072009dbe132bfc573ba3c898ca2d8e
Mochamad Hijul M <32985689+mochijul@users.noreply.github.com>
2023-03-27 22:38:51 +0700

    setup migration

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,20 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class RoleSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(RoleSeeder::class);
-
+        Role::create(['name'=>'user']);
+        Role::create(['name'=>'admin']);
     }
 }
