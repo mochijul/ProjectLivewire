0b52ca932334cad2b8f1ced8c968a44761207b4b
hijul1998@gmail.com <Mochamad Hijul M>
2023-03-29 12:19:51 +0700

    fix spelling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{env('APP_NAME')}}</title>
-</head>
-<body>
-    <h1>Hello, Welcome to this application</h1>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+    <div class="container align-middle">
+        <nav aria-label="breadcrumb ">
+            <ol class="breadcrumb mt-5 d-flex justify-content-center">
+                <h2>Hello, Welcome to this application</h2>
+            </ol>
+        </nav>
+        <p style="text-align: center">already have an account? <a class="mt-4" href="{{ route('login') }}">Login</a></p>
+
+
+        <p style="text-align: center">don't have an account ? <a class="mt-4" href="{{ route('register') }}">Register</a></p>
+        <p style="font-size: 13px">
+            <b>User</b> email:user@user.com password:user <br>
+            <b>Admin</b> email:admin@admin.com password:admin <br>
+            <b>Superadmin</b> email:superadmin@superadmin.com password:superadmin
+        </p>
+    </div>
+@endsection
